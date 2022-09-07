5c538890008d4b1c492776abffc3311074e01459
Bohdan Gurzhiy <bohdan.gurzhiy@gmail.com>
2022-09-07 10:56:21 +0300

    Add project files.

---
+++ TestProject/Controllers/TestController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestProject.Repo;
+
+namespace TestProject.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly TestRepo _testRepo;
+        public TestController(TestRepo testRepo)
+        {
+            _testRepo = testRepo;
+        }
+
+        [HttpGet ("reverse")]
+        public IActionResult Reverse([FromQuery]string data)
+        {
+            if (data == null)
+            {
+                return Ok($"The parameter data is undefined!");
+            }
+            var response = new { result = _testRepo.ReverseOrSqrt(data) };
+            return Ok(response);
+        }
+    }
+}
---
+++ TestProject/Repo/TestRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Repo
+{
+    public class TestRepo
+    {
+        public string Reverse(string data)
+        {
+            return new string(data.Reverse().ToArray());
+        }
+        public string ReverseOrSqrt(string data)
+        {
+            if (double.TryParse(data, out double value))
+            {
+                return Math.Sqrt(value).ToString();
+            }
+            else
+            {
+                return Reverse(data);
+            }
+        }
+    }
+}
